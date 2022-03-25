004d10a659a99fb1c5b39aea55070a7f1333c03c
carolainseija <carolainsilva1@gmail.com>
2022-03-25 11:57:21 -0300

    meals y card componentes

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,36 +2,38 @@ import React from 'react';
 import classes from './AvailableMeals.module.css';
 // import classes from '../UI/Card.js';
 import Card from  '../UI/Card.js';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
         id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
+        name: 'Hamburguesas',
+        description: 'Comunes:',
+        price: 80.00,
     },
     {
         id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
+        name: 'Hamburguesas',
+        description: 'Completas: ',
+        price: 150.00,
     },
     {
         id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
+        name: 'Pizzetas',
+        description: 'panceta, cheddar',
+        price: 240,
     },
     {
         id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
+        name: 'Milanesa Napolitana',
+        description: 'Para uno: Fritas-- Para dos:Ensalada mixta',
+        price: 680.00,
     },
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name}</li>);
+    // const mealsList = DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map((meal)=> <MealItem  key={meal.id} name={meal.name} description={meal.description} price={meal.price} />)
 
     return (
         <section className={classes.meals}>
--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -7,7 +7,7 @@ const MealsSummary = () => {
             <h2>Deliciosa comida, entregada a usted</h2>
             <p>Elija su comida favorita de nuestra amplia selección de comidas
                 disponibles y disfrute de un delicioso almuerzo o cena en casa.</p>
-            <p>Todas nuestras comidas se enfrían con ingredientes de alta calidad,
+            <p>Todas nuestras comidas se cocinan con ingredientes de alta calidad,
                 justo a tiempo y por supuesto, por chefs experimentados.</p>
         </section>
     )
